4b498f1e6df23ebacb59ea4bb6cd591d3df47ed3
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-10-16 14:11:04 +0300

    fixed

--- WinFormsLocalization/Deneme.Designer.cs
+++ WinFormsLocalization/Deneme.Designer.cs
@@ -29,26 +29,22 @@ namespace WinFormsLocalization
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Deneme));
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(302, 127);
+            resources.ApplyResources(this.button1, "button1");
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Clear";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Deneme
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
             this.Name = "Deneme";
-            this.Text = "Deneme";
             this.ResumeLayout(false);
 
         }
